fc80bd5f7a09dbdf104e6fe08d00e1d0e5d0d751
oksana.smila <oksana.smila@exness.com>
2022-02-06 11:02:58 +0200

    Task 1

---
+++ main.py
@@ -0,0 +1,14 @@
+import random
+import task1_calc_sum_diff
+
+# Task 1:
+# Напишите функцию calculations () так, чтобы она могла
+# принимать две переменные и вычислять их сложение и
+# вычитание. А также он должен возвращать как сложение, так
+# и вычитание за один вызов возврата.
+print('Task 1: Calculate sum and diff of 2 random values:')
+test_value_1 = random.randrange(10)
+test_value_2 = random.randrange(10)
+print(f'Value 1 = {test_value_1}, value 2 = {test_value_2}')
+sum, diff = task1_calc_sum_diff.calculations(test_value_1, test_value_2)
+print(f'Sum = {sum}\nDiff = {diff}')
